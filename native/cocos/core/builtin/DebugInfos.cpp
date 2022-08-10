9d9187c63ff172487d5a95a9cc40aac12a7ca095
stanleyljl <798080536@qq.com>
2022-08-10 15:40:45 +0800

    core algorithms for light probe

--- native/cocos/core/builtin/DebugInfos.cpp
+++ native/cocos/core/builtin/DebugInfos.cpp
@@ -453,6 +453,6 @@ ccstd::unordered_map<int, ccstd::string> debugInfos = {
 { 16002, "'%s' is removed since v%s." },
 { 16003, "'%s' is removed since v%s, please use '%s' instead." },
 { 16101, "The effect('%s') you are looking for does not exist, please confirm the effect name in the editor. NOTE: Since 3.6, the name of the built-in effect has been changed to its name in the editor, please check it out. More information please refer to https://docs.cocos.com/creator/manual/en/shader/effect-inspector.html" },
-
+{ 17000, "Less than 4 probes, can not tetrahedralize." },
 };
 }//namespace cc
